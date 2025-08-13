e41b20cfb934317b556cd153b1f30339421d86ff
mfaizk <faiz-react@tarality.com>
2025-08-13 19:34:22 +0530

    marquee removed

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
         <NextGenSection />
         <ChartSection />
         <TimeLineSection />
-        <MarqueeSection />
+        {/* <MarqueeSection /> */}
       </div>
     </div>
   );
